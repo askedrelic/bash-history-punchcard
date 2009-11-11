9bb372d782f8184626ea74b73a96250c9971180f
Matt Behrens <askedrelic@gmail.com>
2009-11-10 20:46:36 -0500

    forgot to add pygooglechart requirement

--- bashpunchcard.py
+++ bashpunchcard.py
@@ -3,8 +3,12 @@ Copyright (C) 2009  Matt Behrens <askedrelic@gmail.com> http://asktherelic.com
 
 Script for creating a github style punchcard of your bash history.
 
-Requires the HISTTIMEFORMAT variable to be set, which includes unix timestamps 
+Requires
+-the HISTTIMEFORMAT variable to be set, which includes unix timestamps 
 on all bash commands.
+
+-pygooglechart
+http://pygooglechart.slowchop.com/
 
 Ideas and most code from:
 http://dustin.github.com/2009/01/11/timecard.html
